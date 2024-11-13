4bd5f12da9a2ba1ac3e13ce6523638de153139f9
Roman Shrestha <romanstha07@gmail.com>
2024-11-12 22:32:39 -0800

    Minor changes to the header.py file

--- modules/header.py
+++ modules/header.py
@@ -6,4 +6,6 @@ def header():
 
     ## Title & details
     st.title("Assignment 3")
-    st.subheader("A simple application to perform CRUD operations on the Customer table of a Car database.")
+    st.subheader("Roman Shrestha | 100414361 | CPSC-4820-001")
+    st.write(
+        "A simple application made with Streamlit to perform CRUD operations on the Customer table of a Car database.")
